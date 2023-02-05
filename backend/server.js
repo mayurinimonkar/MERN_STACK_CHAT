8b3049032baf790b4e2e387a205799cea619ec89
Mayuri Nimonkar <mayuri.nimonkar@aventior.com>
2023-02-05 19:38:59 +0530

    User controller

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ const { chats } = require("./data/data");
 const dotenv = require("dotenv")
 const cors = require('cors');
 const connectDB = require("./config/db");
-const colors = require("colors")
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes")
 
 dotenv.config();
 connectDB();
@@ -16,16 +17,17 @@ app.get("/", (req,res)=>{
     res.send("Api is running")
 })
 
-app.get("/api/chat", (req, res)=>{
-    res.send(chats)
-})
+app.use("api/user", userRoutes)
+// app.get("/api/chat", (req, res)=>{
+//     res.send(chats)
+// })
 
-app.get("/api/chat/:id", (req,res)=>{
-    const singleChat = chats.find((c)=>c._id === req.params.id)
+// app.get("/api/chat/:id", (req,res)=>{
+//     const singleChat = chats.find((c)=>c._id === req.params.id)
 
-    res.send(singleChat);
+//     res.send(singleChat);
 
-})
+// })
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,  console.log(`Server is listening on port ${PORT}`.yellow.bold));
