a635500ad58f597efece321e57ff0c81bf358eda
Mayuri Nimonkar <mayuri.nimonkar@aventior.com>
2023-02-05 21:22:16 +0530

    API error handlers

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
@@ -19,6 +20,8 @@ app.get("/", (req,res)=>{
 })
 
 app.use("/api/user", userRoutes)
+app.use(notFound)
+app.use(errorHandler)
 // app.get("/api/chat", (req, res)=>{
 //     res.send(chats)
 // })
