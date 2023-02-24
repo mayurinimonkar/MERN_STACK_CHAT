0c5a91bc886628cd73943d988602624c20cc9e52
Mayuri Nimonkar <mayuri.nimonkar@aventior.com>
2023-02-24 22:36:56 +0530

    Chat API created

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes")
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
@@ -20,6 +21,7 @@ app.get("/", (req,res)=>{
 })
 
 app.use("/api/user", userRoutes)
+app.use("/api/chat", chatRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
