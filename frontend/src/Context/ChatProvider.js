0223af41ce606a8e899c4fcda09f2d8916612d13
Mayuri Nimonkar <mayuri.nimonkar@aventior.com>
2024-03-02 16:51:32 +0530

    Search User Side Drawer

--- frontend/src/Context/ChatProvider.js
+++ frontend/src/Context/ChatProvider.js
@@ -1,32 +1,32 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 const ChatContext = createContext();
-const ChatProvider = ({children}) =>{
+const ChatProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [selectedChat, setSelectedChat] = useState();
+  const [chats, setChats] = useState();
+  const history = useHistory();
 
-    const [user, setUser] = useState()
-    const history = useHistory()
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    setUser(userInfo);
 
-    useEffect(() => {
-      const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-      setUser(userInfo)
-      
-      if(!userInfo){
-        history.push("/")
-        
-      }
-    }, [history])
-    
-    return ( 
-        <ChatContext.Provider value={{user, setUser}}>
-            { children}
-        </ChatContext.Provider>
-    )
-}
+    if (!userInfo) {
+      history.push("/");
+    }
+  }, [history]);
+
+  return (
+    <ChatContext.Provider
+      value={{ user, setUser, selectedChat, setSelectedChat, chats, setChats }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
 
 export const ChatState = () => {
-
- return useContext(ChatContext)
-}
-
+  return useContext(ChatContext);
+};
 
 export default ChatProvider;
